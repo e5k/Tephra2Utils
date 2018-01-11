daa754fc7ffe3d6fc135b6a8b7e29a801c711085
e5k <seb@eskd.net>
2018-01-10 16:25:15 -1000

    progresses

---
+++ makeBeta.m
@@ -0,0 +1,52 @@
+% function MAKEBETA(alpha, beta, height)
+% Plot the probability of mass release from the plume following a Beta
+% distribution.
+% - Alpha and Beta can be entered as single values or as vectors.
+% - The height can be entered either as [min max] or as max, in which case
+%   the beta distribution is calculated from ground level.
+% 
+% Examples
+%   makeBeta(3, 2, 10)
+%   makeBeta([3,3,3],[1,2,3],[1,10])
+%
+% Written by S. Biass, Jan 2018
+
+function makeBeta(alpha, beta, height)
+
+if numel(height) == 1
+    height = [0, height];
+end
+
+if numel(alpha) ~= numel(beta)
+    error('Alpha and beta should have the same size')
+end
+
+x = linspace(0,1,50);
+h = height(1) + x.*(height(2)-height(1));
+
+stor = zeros(numel(x), numel(alpha), 2);
+leg  = cell(numel(alpha),1);
+for i = 1:numel(alpha)
+    stor(:,i,1) = betapdf(x,alpha(i), beta(i))';
+    stor(:,i,1) = stor(:,i,1)./sum(stor(:,i,1));
+    stor(:,i,2) = betacdf(x,alpha(i), beta(i))';
+    leg{i}      = ['\alpha = ', num2str(alpha(i)), ', \beta = ', num2str(beta(i))];
+end
+
+figure;
+subplot(2,1,1)
+plot(stor(:,:,1).*100, h);
+grid on
+xlabel('Probability of release (%)');
+ylabel('Plume height')
+legend(leg)
+grid on
+title('PDF')
+
+subplot(2,1,2)
+plot(stor(:,:,2).*100, h);
+grid on
+xlabel('Cummulative robability of release (%)');
+ylabel('Plume height')
+grid on
+title('CDF')
--- runT2.m
+++ runT2.m
@@ -1,16 +1,16 @@
 function runT2(varargin)
 
 run.name    = 'test';
-run.mode    = 2;                % 0: Single run
-                                % 1: Random
-                                % 2: Sensitivity
-run.mass    = 0;                % 0: Mass computed from height, wind and duration
-                                % 1: Mass if specified
+run.mode    = 0;                % 0: Deterministic
+                                % 1: Probabilistic
+run.mass    = 0;                % 0: Mass if specified
+                                % 1: Mass computed from height, wind and duration
+                                % 2: Mass computed from thermal theory - to add
                                 
 grid.xLim   = [-50000 250000];  % X limits (UTM, m)
 grid.yLim   = [-75000 75000];   % Y limits (UTM, m)
 grid.res    = [1000,2000];             % Grid resolution (m)
-grid.elev   = 0;                % Mean elevation
+grid.elev   = [0,10];                % Mean elevation
 grid.zone   = [];               % Zone
 
 vent.x      = 0;
@@ -32,20 +32,18 @@ tgsd.agg    = 0.5;
 tgsd.minD   = 5;
 
 
-conf.plume_height   = [1000, 2500, 5000:5000:30000];
-conf.mass           = [];
-conf.duration       = 1; % Hours
-conf.eddy_const     = 0.04;
-conf.diff_coef      = 112;
-conf.ftt            = 1563;
-conf.lit_dens       = 2600;
-conf.pum_dens       = 513;
-conf.col_step       = 50;
-conf.part_step      = 50;
-conf.plume_model    = 0;
-conf.plume_ratio    = .27;
-conf.plume_alpha    = 3;
-conf.plume_beta     = 2;
+conf.plumeHeight   = [1000, 2500, 5000:5000:30000];
+conf.mass          = 1e10;
+conf.duration      = 1; % Hours
+conf.eddyConst     = 0.04;
+conf.diffCoef      = 112;
+conf.ftt           = 1563;
+conf.lithDens      = [2600,3000];
+conf.pumDens       = 513;
+conf.colStep       = 50;
+conf.partStep      = 50;
+conf.plumeAlpha    = 3;
+conf.plumeBeta     = 2;
 
 
 %% Prepare output
@@ -56,22 +54,17 @@ mkdir(fullfile(run.name, 'WIND'));
 mkdir(fullfile(run.name, 'OUT'));
 mkdir(fullfile(run.name, 'TGSD'));
 
-elements    = cell(1000,1);
-elementsN   = cell(1000,1);
+elements    = cell(30,1);
+elementsN   = cell(30,1);
 elemC       = 1;
 
 %% Grid
-elem    = {'res', 'elev'};
-elemF   = {'Grid resolution', 'Grid elevation'};
-elemS   = elemC;
-for i = 1:length(elem)
-    elements{elemC}     = grid.(elem{i});
-    elementsN{elemC}    = elemF{i};
-    elemC               = elemC+1;
-end
-grid.P  = makePossibilities(elements(elemS:elemC-1));   
-grid.N  = cellfun(@num2str, num2cell(grid.P), 'UniformOutput', false);
-grid.N  = makeName(grid.N, elem, 'grid', '.grd');
+grid.elem    = {'res', 'elev'};                          % Variable name
+grid.elemF   = {'Grid resolution', 'Grid elevation'};    % Full name
+elemS   = elemC;                                    % Start counter
+
+[elements, elementsN, elemC, grid] ...
+        = prepareInput(elements, elementsN, elemS, elemC, grid, 'grid', '.grd');
 
 for i = 1:size(grid.P,1)
     makeGrid(grid.xLim(1):grid.P(i,1):grid.xLim(2),...
@@ -82,17 +75,12 @@ end
 
 %% Wind
 if isempty(wind.pth)
-    elem    = {'dir', 'vel', 'trop', 'strat', 'model'};
-    elemF   = {'Wind direction', 'Wind velocity', 'Tropopause height', 'Stratosphere height', 'Wind model'};
-    elemS   = elemC;
-    for i = 1:length(elem)
-        elements{elemC}     = wind.(elem{i});
-        elementsN{elemC}    = elemF{i};
-        elemC               = elemC+1;
-    end
-    wind.P  = makePossibilities(elements(elemS:elemC-1));
-    wind.N  = cellfun(@num2str, num2cell(wind.P), 'UniformOutput', false);
-    wind.N  = makeName(wind.N, elem, 'wind', '.wnd');
+    wind.elem    = {'dir', 'vel', 'trop', 'strat', 'model'}; % Variable name
+    wind.elemF   = {'Wind direction', 'Wind velocity', 'Tropopause height', 'Stratosphere height', 'Wind model'};
+    elemS   = elemC; % Start counter
+
+    [elements, elementsN, elemC, wind] ...
+            = prepareInput(elements, elementsN, elemS, elemC, wind, 'wind', '.wnd');
     
     for i = 1:size(wind.P,1)
         makeWindProfile(wind.P(i,1), wind.P(i,2), wind.P(i,3), wind.P(i,4), wind.P(i,5), fullfile(run.name, 'WIND', wind.N{i}));
@@ -101,36 +89,127 @@ end
     
 %% TGSD
 if isempty(tgsd.pth)
-    elem    = {'med', 'std', 'agg', 'minD'};
-    elemF   = {'Median TGSD', 'Std TGSD', 'Aggregation coef.', 'Min aggregate diam'};
+    tgsd.elem    = {'med', 'std', 'agg', 'minD'};
+    tgsd.elemF   = {'Median TGSD', 'Std TGSD', 'Aggregation coef.', 'Min aggregate diam'};
     elemS   = elemC;
-    for i = 1:length(elem)
-        elements{elemC}     = tgsd.(elem{i});
-        elementsN{elemC}    = elemF{i};
-        elemC               = elemC+1;
 
-        tgsd.P  = makePossibilities(elements(elemS:elemC-1));
-        tgsd.N  = cellfun(@num2str, num2cell(tgsd.P), 'UniformOutput', false);
-        tgsd.N  = makeName(tgsd.N, elem, 'tgsd', '.gsd');
-    end
+    [elements, elementsN, elemC, tgsd] ...
+            = prepareInput(elements, elementsN, elemS, elemC, tgsd, 'tgsd', '.gsd');
     
     for i = 1:size(tgsd.P,1)
         makeTGSD(tgsd.P(i,1), tgsd.P(i,2), tgsd.P(i,3), tgsd.P(i,4), fullfile(run.name, 'TGSD', tgsd.N{i}));
     end
 end
 
-% Configuration file
-%elem    = {'plume_height', 'mass', 'duration', 'eddy_const', 'diff_coef', 'ftt', 'lit_dens', 'pum_dens', 'col_step', 'part_step', 'plume_model', 'plume_ratio', 'plume_alpha', 'plume_beta'};
-elem    = {'plume_height', 'mass', 'duration', 'eddy_const', 'diff_coef', 'ftt', 'lit_dens', 'pum_dens', 'col_step', 'part_step'};
-elemF   = {'Plume height', 'Mass', 'Duration', 'Eddy constant', 'Diffusion coef', 'FTT', 'Lithic density', 'Pumice density', 'Column steps', 'Particle steps'};
+%% Configuration file
+%elem    = {'plumeHeight', 'mass', 'duration', 'eddyConst', 'diffCoef', 'ftt', 'lithDens', 'pumDens', 'colStep', 'partStep', 'plume_model', 'plume_ratio', 'plumeAlpha', 'plumeBeta'};
+% Append wind to
+conf.windVel    = wind.vel; 
+conf.windTrop   = wind.trop; 
+
+var     = {'plumeHeight', 'mass', 'duration', 'eddyConst', 'diffCoef', 'ftt', 'lithDens', 'pumDens', 'colStep', 'partStep', 'plumeAlpha', 'plumeBeta', 'windVel','windTrop'};
+varF    = {'Plume height', 'Mass', 'Duration', 'Eddy constant', 'Diffusion coef', 'FTT', 'Lithic density', 'Pumice density', 'Column steps', 'Particle steps', 'Alpha', 'Beta', 'wind', 'trop'};
+
+% Index used to define what variables are associated for a given run.ass
+if run.mass == 0
+    idxE         = ~strcmp(var, 'duration');
+elseif run.mass == 1
+    idxE         = ~strcmp(var, 'mass');
+end
+
+% Here, generate a first batch of possibilities by considering permutations
+% of wind velociy and duration
+conf.elem    = var(idxE);
+conf.elemF   = varF(idxE);
+elemS   = elemC;
+
+% [elements, elementsN, elemC, conf] ...
+%     = prepareInput(elements, elementsN, elemS, elemC, conf, 'conf', '.conf');
+
+[~,~,~, conf] ...
+    = prepareInput(elements, elementsN, elemS, elemC, conf, 'conf', '.conf');
+
+% Calculate MER
+MER = zeros(size(conf.P,1),1);
+for i = 1:size(conf.P,1)
+    MER(i,1) = get_MER_DB12(conf.P(i, strcmp(var(idxE),'plumeHeight'))-vent.z, ...
+        conf.P(i, strcmp(var(idxE),'windVel')),...
+        conf.P(i, strcmp(var(idxE),'windTrop')));
+end
+
+% Remove the wind vectors associated with conf file from the global elements
+
+conf.elem  = conf.elem(1:end-2);
+conf.elemF = conf.elemF(1:end-2);
+
+% Calculate mass and duration as a function of run.mass
+if run.mass == 0
+    duration    = conf.P(:, strcmp(var(idxE),'mass')) ./ MER ./ 3600;
+    conf.P      = [conf.P, [MER, duration]];
+    conf.elem   = [conf.elem, [{'MER'}, {'duration'}]];
+    conf.elemF  = [conf.elemF, [{'MER'}, {'Duration'}]];
+elseif run.mass == 1
+    mass        = MER .* conf.P(:, strcmp(var(idxE),'windVel')).*3600;
+    conf.P      = [conf.P, [MER, mass]];
+    conf.elem   = [conf.elem, [{'MER'}, {'mass'}]];
+    conf.elemF  = [conf.elemF, [{'MER'}, {'Mass'}]];
+end
+    
+% generate a second bacth of possibilities for permutations on variables
+% only relevant for the generation of configuration files
+idx = ~(strcmp(conf.elem, 'MER') | strcmp(conf.elem, 'duration'));
+conf.elem  = conf.elem(idx);
+conf.elemF = conf.elemF(idx);
+
+[elements, elementsN, elemC, conf] ...
+    = prepareInput(elements, elementsN, elemS, elemC, conf, 'conf', '.conf');
+
+
+% Stopped here. elements now contain all variable required to generate the
+% permutations for conf files. Now still need to figure out how the indices
+% of mass/height/wind can be used to later reshape and include MER and
+% duration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+elementsN = elementsN(~cellfun(@isempty, elements));
+elements  = elements(~cellfun(@isempty, elements));
+projectRun = makePossibilities(elements);
+projectRun = mat2cell(projectRun, 192, ones(size(projectRun,2),1));
+
+
+
+for i = 1:size(conf.P,1)
+    makeConf(conf.P(i,1), conf.P(i,2), conf.P(i,3), tgsd.P(i,4), fullfile(run.name, 'TGSD', tgsd.N{i}));
+end
+
 
 % In case run mode is either single or sensitivity, remove reference to
 % either mass or volume according to run.mass
 % NEED TO ADAPT FOR PROBABILISTIC
-if (run.mode == 0 || run.mode == 2) && run.mass == 0
+if run.mode == 0 && run.mass == 1
     elem(2)     = [];
     elemF(2)    = [];
-elseif (run.mode == 0 || run.mode == 2) && run.mass == 1  
+elseif run.mode == 0 && run.mass == 0  
     elem(3)     = [];
     elemF(3)    = []; 
 end
@@ -152,10 +231,22 @@ if run.mass == 0
     
 end
 
+% For each of grid, conf, gs and wind, return the all the combinations of
+% variables and generate file names
+function  [elements, elementsN, elemC, struc] = prepareInput(elements, elementsN, elemS, elemC, struc, fl, ext)
+for i = 1:length(struc.elem)
+    elements{elemC}     = struc.(struc.elem{i});
+    elementsN{elemC}    = struc.elemF{i};
+    elemC               = elemC+1;
+end
+struc.P  = makePossibilities(elements(elemS:elemC-1));   
+struc.N  = cellfun(@num2str, num2cell(struc.P), 'UniformOutput', false);
+struc.N  = makeName(struc.N, struc.elem, fl, ext);
+
 % Create file names
 function outN = makeName(N, elem, fl, ext)
 for i = 1:size(N,2)
-    N(:,i) = strcat(['_', elem{i}], N(:,i));
+    N(:,i) = strcat(['_', elem{i}], '-', N(:,i));
 end
 outN = cell(size(N,1),1);
 for i = 1:size(N,1)
@@ -163,10 +254,6 @@ for i = 1:size(N,1)
 end
 
 
-function makeGrid(xVec,yVec,elev,name)
-[X,Y] = meshgrid(xVec, yVec);
-utm   = [reshape(X, numel(X),1), reshape(Y, numel(X),1), reshape(ones(numel(X),1).*elev, numel(X),1)];
-dlmwrite(name, utm, 'delimiter', '\t')
 
 function result = makePossibilities(elements)
 combinations        = cell(1, numel(elements)); %set up the varargout result
@@ -174,7 +261,14 @@ combinations        = cell(1, numel(elements)); %set up the varargout result
 combinations        = cellfun(@(x) x(:), combinations,'uniformoutput',false); %there may be a better way to do this
 result              = [combinations{:}]; % NumberOfCombinations by N matrix. Each row is uni
 
+% % Generation of input parameters
+% Create grid
+function makeGrid(xVec,yVec,elev,name)
+[X,Y] = meshgrid(xVec, yVec);
+utm   = [reshape(X, numel(X),1), reshape(Y, numel(X),1), reshape(ones(numel(X),1).*elev, numel(X),1)];
+dlmwrite(name, utm, 'delimiter', '\t')
 
+% Create wind profiles
 function makeWindProfile(dir, speed, trop, strat, model, name)
 dummyH      = [0:50:500, 600:100:2000, 2500:500:10000, 11000:1000:25000, 27500:2500:40000];
 dummyH(1)   = 1; % Remove a 0 elevation
@@ -195,7 +289,7 @@ else
 end
 dlmwrite(name, [dummyH', vel', ones(size(vel')).*dir], 'delimiter', '\t');
 
-
+% Create TGSD
 function makeTGSD(med, std, agg, maxD, name)
 
 diam    = -15:15;
@@ -203,21 +297,36 @@ i1      = diam >= -1 & diam < maxD;
 i2      = diam >= maxD & diam <= diam(end);
 
 wt      = normpdf(diam, med, std);
+%wt      = round(wt,4);
 
 wt(i2)  = wt(i2) - wt(i2)*agg;
-wt(i1)  = wt(i1) + sum(wt(i2))*agg/nnz(i1);
+wt(i1)  = wt(i1) + sum(wt(i2).*agg)/nnz(i1);
 wt      = round(wt,4);
+diam    = diam(wt>0);
+wt      = wt(wt>0); 
 
 % Convert to cdf
 for i = 2:length(wt)
    wt(i) = wt(i-1)+wt(i); 
 end
-diam    = diam(wt>0);
-wt      = wt(wt>0); 
+
+% Correct rounding
+wt(wt==wt(end)) = 1;
 
 dlmwrite(name, [diam', wt'], 'delimiter', '\t', 'precision', 4);
 
 
+function makeConf
+
+fid = fopen(fullfile(out_pth, 'CONF', seas_str{seas}, num2str(i), [num2str(j,'%04d'), '.conf']), 'wt');
+fprintf(fid,...
+    'PLUME_HEIGHT\t%d\nERUPTION_MASS\t%d\nVENT_EASTING\t%d\nVENT_NORTHING\t%d\nVENT_ELEVATION\t%d\nEDDY_CONST\t%d\nDIFFUSION_COEFFICIENT\t%d\nFALL_TIME_THRESHOLD\t%d\nLITHIC_DENSITY\t%d\nPUMICE_DENSITY\t%d\nCOL_STEPS\t%d\nPART_STEPS\t%d\nPLUME_MODEL\t%d\nALPHA\t%d\nBETA\t%d\n',...
+    ht_tmp(j), mass_tmp(j),...
+    data.vent_easting, data.vent_northing, data.vent_ht,...
+    data.eddy_const, data.diff_coeff, data.ft_thresh,...
+    data.lithic_dens, data.pumice_dens,...
+    data.col_step, data.part_step, 2, data.alpha, data.beta);
+fclose(fid);
 % 
 % 
 % 
